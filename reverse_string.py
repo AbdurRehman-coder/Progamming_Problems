7ef685e8e0cd39829940c667c0e96cd18c9de3e8
Abdur Rehman <abdurrehmanlkl786@gmail.com>
2022-04-27 20:56:30 +0500

    Reverse the given String

---
+++ reverse_string.py
@@ -0,0 +1,21 @@
+# Problem: You are given a string s. You need to reverse the string.
+# Like, Hello into olleH
+
+class StringProblem:
+    #This method will reverse the string
+    # e.g: 'Hello' into 'olleH'
+    def reverseString(sefl, strValue):
+        # empty variable to store the returned reverst char
+        reversed = ''
+        end = len(strValue) -1; # Length of that String
+        # Run this loop until it reach into 0
+        while(end >= 0):
+            reversed = reversed + strValue[end]
+            end -= 1
+        return reversed;
+
+obj = StringProblem()
+
+strvalue = 'GeeksForGeeks' 
+print(obj.reverseString(strvalue))  
+print(obj.reverseString('Hello'))
